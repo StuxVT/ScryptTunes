e1bf77d104e5a699b11f29e47819c9239be8991a
Stux <100985218+StuxVT@users.noreply.github.com>
2023-09-05 00:22:54 -0400

    added a way to input entries to a list, themed to blue

--- ui/frames/settings_frame.py
+++ ui/frames/settings_frame.py
@@ -1,5 +1,6 @@
 from customtkinter import CTkFrame, CTkEntry, CTkButton
 
+from ui.frames.list_input import ListInput
 
 class SettingsFrame(CTkFrame):
     def __init__(self, master, settings_controller):
@@ -11,8 +12,12 @@ class SettingsFrame(CTkFrame):
         self.setting_entry.insert(0, settings_controller.get("nickname"))
         self.save_button = CTkButton(self, text="Save", command=self.save_setting)
 
+        self.list_entry = ListInput(self)
+
         self.setting_entry.pack()
         self.save_button.pack()
+        self.list_entry.pack()
+
 
     def save_setting(self):
         new_value = self.setting_entry.get()
