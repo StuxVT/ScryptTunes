390cf0e322758c5c1630c442abb9b2a2b3e40276
Stux <100985218+StuxVT@users.noreply.github.com>
2023-09-11 08:03:46 -0400

    [High-Priority] Fail to close event loop (already closed) prevents app close

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import logging
-from logging.handlers import RotatingFileHandler  # Import RotatingFileHandler
-from rich.logging import RichHandler
-from ui.main_app import MainApp
+import subprocess
+from logging.handlers import RotatingFileHandler
 
 
 def setup_logging():
@@ -18,11 +17,24 @@ def setup_logging():
 
 
 def main():
+    # only import deps after insuring installed
+    try:
+        from rich.logging import RichHandler
+        from ui.main_app import MainApp
+    except ImportError:
+        install_requirements()
+        from rich.logging import RichHandler
+        from ui.main_app import MainApp
+
     setup_logging()
     logging.info("Application started")
 
     root = MainApp()
     root.mainloop()
+
+
+def install_requirements():
+    subprocess.run(["pip", "install", "-r", "requirements.txt"])
 
 
 if __name__ == "__main__":
