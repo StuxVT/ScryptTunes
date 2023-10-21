b9bc9a5318fdfe17057a2b6668b4ae18c99c8fb2
Stux <100985218+StuxVT@users.noreply.github.com>
2023-10-20 23:07:22 -0400

    Various updates before official release v1 (#35) (#36)
    - UI updates
    - settings changes
    - Readme

--- ui/frames/settings_frame.py
+++ ui/frames/settings_frame.py
@@ -13,8 +13,8 @@ class SettingsFrame(CTkFrame):
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)  # Configure row 0 to expand vertically
 
-        # Nickname
-        self.nickname_row = TextSettingRow(self, setting_name="Nickname",
+        # Bot account
+        self.nickname_row = TextSettingRow(self, setting_name="Bot Account",
                                            setting_description="The username of your bot account",
                                            initial_value=settings_controller.get("nickname"))
         self.nickname_row.grid(row=0, column=0, padx=10, pady=5, sticky="ew")
@@ -25,11 +25,11 @@ class SettingsFrame(CTkFrame):
                                          initial_value=settings_controller.get("prefix"))
         self.prefix_row.grid(row=1, column=0, padx=10, pady=5, sticky="ew")
 
-        # Channels
-        self.channels_row = ListSettingRow(self, setting_name="Channels",
-                                           setting_description="List of channels for your bot to look for commands",
-                                           initial_value=settings_controller.get("channels"))
-        self.channels_row.grid(row=2, column=0, padx=10, pady=5, sticky="ew")
+        # Watch-Channel
+        self.channel_row = TextSettingRow(self, setting_name="Watch Channel",
+                                          setting_description="Your twitch channel",
+                                          initial_value=settings_controller.get("channel"))
+        self.channel_row.grid(row=2, column=0, padx=10, pady=5, sticky="ew")
 
         # Token
         self.token_row = TextSettingRow(self, setting_name="Token",
@@ -72,13 +72,6 @@ class SettingsFrame(CTkFrame):
                                              hidden=True)
         self.spotify_secret.grid(row=8, column=0, padx=10, pady=5, sticky="ew")
 
-        # spotify_redirect_uri
-        self.spotify_redirect_uri = TextSettingRow(self, setting_name="spotify_redirect_uri",
-                                                   setting_description="Redirect URL configured in Spotify dev portal",
-                                                   initial_value=settings_controller.get("spotify_redirect_uri"),
-                                                   hidden=True)
-        self.spotify_redirect_uri.grid(row=9, column=0, padx=10, pady=5, sticky="ew")
-
         # Save Settings
         self.save_button = CTkButton(self, text="Save", command=self.save_settings)
         self.save_button.grid(row=10, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
@@ -86,13 +79,12 @@ class SettingsFrame(CTkFrame):
     def save_settings(self):
         self.settings_controller.set('nickname', self.nickname_row.get())
         self.settings_controller.set('prefix', self.prefix_row.get())
-        self.settings_controller.set('channels', self.channels_row.get())
+        self.settings_controller.set('channel', self.channel_row.get())
         self.settings_controller.set('token', self.token_row.get())
         self.settings_controller.set('client_id', self.client_id_row.get())
         self.settings_controller.set('client_secret', self.client_secret.get())
         self.settings_controller.set('channel_points_reward', self.channel_points_reward.get())
         self.settings_controller.set('spotify_client_id', self.spotify_client_id.get())
         self.settings_controller.set('spotify_secret', self.spotify_secret.get())
-        self.settings_controller.set('spotify_redirect_uri', self.spotify_redirect_uri.get())
 
         self.settings_controller.save_config()
