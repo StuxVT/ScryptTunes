856e5ac64b25887930eb145249fd2dc49228c3c2
NickDiam <diamantonikolas@gmail.com>
2024-04-21 01:02:30 +0200

    Fixed getting bot version

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -10,12 +10,14 @@ def get_bot_version() -> str:
     # If this is not the case then this SHOULD CHANGE!
 
     # Parent directory of bot module
-    dir_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    dir_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     try:
+        print(dir_path)
         with open(os.path.join(dir_path, "VERSION")) as version_file:
             return version_file.read().strip()
 
     except FileNotFoundError:
         logging.warning("Could not fetch VERSION file")
     return "0.0"
+
 
