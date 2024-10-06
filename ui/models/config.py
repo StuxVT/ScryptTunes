42dbb07482e0e30e0e5318e7295601a02a2e5cf4
Stux <100985218+StuxVT@users.noreply.github.com>
2024-10-06 00:43:18 -0400

    try applying a default config

--- ui/models/config.py
+++ ui/models/config.py
@@ -7,8 +7,8 @@ class PermissionConfig(BaseModel):
     unsubbed: bool = False
     subscriber: bool = False
     vip: bool = False
-    mod: bool = False
-    broadcaster: bool = False
+    mod: bool = True
+    broadcaster: bool = True
 
 
 class PermissionSetting(BaseModel):
@@ -34,6 +34,27 @@ class Config(BaseModel):
     channel_points_reward: str = ""
     spotify_client_id: str = ""
     spotify_secret: str = ""
-    spotify_redirect_uri: str = ""
+    spotify_redirect_uri: str = "http://localhost:8080"
     rate_limit: int = 0
-    permissions: PermissionSettingDict  # todo: dynamically build dict based on PermissionSetting data
+    permissions: PermissionSettingDict = PermissionSettingDict(
+        ping_command=PermissionSetting(
+            command_name="ping_command",
+            permission_config=PermissionConfig()
+        ),
+        np_command=PermissionSetting(
+            command_name="np_command",
+            permission_config=PermissionConfig()
+        ),
+        queue_command=PermissionSetting(
+            command_name="queue_command",
+            permission_config=PermissionConfig()
+        ),
+        recent_played_command=PermissionSetting(
+            command_name="recent_played_command",
+            permission_config=PermissionConfig()
+        ),
+        songrequest_command=PermissionSetting(
+            command_name="songrequest_command",
+            permission_config=PermissionConfig()
+        )
+    )
